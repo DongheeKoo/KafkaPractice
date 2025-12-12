029cebe7521d6fc3ac71d436815438a323925c4a
DongheeKoo <dev.dongheekoo1995@gmail.com>
2025-12-12 21:57:29 +0900

    [REFACTOR] EC2용 보안그룹이 모든 아웃바운드 트래픽을 허용하도록 수정

--- modules/ec2/sg.tf
+++ modules/ec2/sg.tf
@@ -16,6 +16,13 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["172.31.0.0/16"]
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name      = "${var.env_prefix}-sg-kafka"
     ManagedBy = "Terraform"
